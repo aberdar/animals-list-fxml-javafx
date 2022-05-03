33372d9b17dc735eace817c5fb4cdd81586b1982
aberdar <sashaberdar@icloud.com>
2022-05-03 16:36:04 +0300

    Adding main class

--- src/main/java/personal/application/Main.java
+++ src/main/java/personal/application/Main.java
@@ -1,13 +1,35 @@
 package personal.application;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+    private final ObservableList<Animal> animalsData = FXCollections.observableArrayList(
+            new Animal("Dog", "Mike", 5, 3, "Alexander"),
+            new Animal("Cat", "Mars", 5, 7, "Victoria")
+    );
 
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("MainWindow.fxml"));
+            MainWindowController controller = loader.getController();
+//            controller.setData(animalsData);
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Animals list");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 
     public static void main(String[] args){
