148f5adb26c2c93c3fbdea6fae5760acfbffa57e
aberdar <sashaberdar@icloud.com>
2022-05-05 21:02:19 +0300

    Code refactor

--- src/main/java/personal/AnimalAddDialogController.java
+++ src/main/java/personal/AnimalAddDialogController.java
@@ -1,11 +1,8 @@
 package personal;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.DialogPane;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
--- src/main/java/personal/Main.java
+++ src/main/java/personal/Main.java
@@ -8,9 +8,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.net.URL;
-
 public class Main extends Application {
 
     private final ObservableList<Animal> animalsData = FXCollections.observableArrayList(
